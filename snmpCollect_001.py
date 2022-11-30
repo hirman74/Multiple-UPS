9826aec487f14a13483113ea0514e9ae893b8cf4
hirman <hirman74@users.noreply.github.com>
2022-11-30 20:53:25 +0800

    Update snmpCollect_001.py

--- snmpCollect_001.py
+++ snmpCollect_001.py
@@ -2,38 +2,59 @@
 import os
 import json
 import time
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, check_output
 # subprocess.run(args, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None, **other_popen_kwargs)
 
 def secondsTime():
     getHourMinuteSeconds = time.strftime("%Y%b%d_%H%M%S")
     return getHourMinuteSeconds
 
-def checkLogFileSize(filename):
-    if os.path.getsize(filename) > 1000:
+def checkLogFileSize(resultedFile):
+    if os.path.getsize(resultedFile) > 1000:
         return True
 
-def collectingData(host):
-    oidToCheck = ['upsBaseBatteryTimeOnBattery','upsSecondsOnBattery','upsSmartInputLineVoltage', 'upsInputVoltage1']
-    for eachOIDName, thisDict in snmpMIBS.items(): #name:value pair
-        if eachOIDName in oidToCheck:
+def checkBattery(host, snmpResults):
+    # upsBatteryStatus == .1.3.6.1.2.1.33.1.2.1.0
+    # INTEGER {unknown(1),batteryNormal(2),batteryLow(3),batteryDepleted(4)
+    #snmpFolder = 'C:\\Users\\16899486_admin\\Desktop\\C830G\\UPS\\Net-SNMP-Install\\bin\\'
+    snmpFolder = '/usr/bin/'
+    snmpCMD = [snmpFolder + 'snmpget']
+    snmpARG = ['-v', '2c', '-c', 'public', '-OQ', '-t', '10', '-r', '2']
+    for str in snmpARG:
+        snmpCMD.append(str)
 
-            snmpCMD.append(host.strip())
-            snmpCMD.append(thisDict['oidString'])
+    snmpCMD.append(host.strip())
+    snmpCMD.append('.1.3.6.1.2.1.33.1.2.1.0')
+    querySNMP = Popen( snmpCMD , stdout=PIPE, stderr=PIPE)
+    stdout, stderr = querySNMP.communicate()
 
-            querySNMP = Popen( snmpCMD , stdout=PIPE, stderr=PIPE)
-            stdout, stderr = querySNMP.communicate()
-
-            snmpValue = stdout.split(':')[-1]
-            snmpErrors = stderr.split(':')[-1]
-            # print(snmpValue)
-            # print(snmpErrors)
-            snmpResults = updateResult(host, snmpValue, eachOIDName)
+    if len(stdout) > 0:
+        snmpValue = stdout.decode('utf-8').split('=')[-1].replace('\n', '').strip()
+        snmpResults['hostIP'] = host
+        if snmpValue == '1' :
+            snmpResults['upsBatteryStatus']['status'] = 'unknown'
+            snmpResults['upsBatteryStatus']['value'] = snmpValue.strip()
+        elif snmpValue == '2' :
+            snmpResults['upsBatteryStatus']['status'] = 'batteryNormal'
+            snmpResults['upsBatteryStatus']['value'] = snmpValue.strip()
+        elif snmpValue == '3' :
+            snmpResults['upsBatteryStatus']['status'] = 'batteryLow'
+            snmpResults['upsBatteryStatus']['value'] = snmpValue.strip()
+        elif snmpValue == '4' :
+            snmpResults['upsBatteryStatus']['status'] = 'batteryDepleted'
+            snmpResults['upsBatteryStatus']['value'] = snmpValue.strip()             
+    elif len(stderr) > 0:
+        snmpError = stderr.decode('utf-8').split('=')[-1].replace('\n', '').strip()
+        snmpResults['hostIP'] = host
+        if 'No Response from' in snmpError:
+            snmpResults['upsBatteryStatus']['status'] = 'DOWN/INVALID'
+            snmpResults['upsBatteryStatus']['value'] = snmpError.strip()
     return snmpResults
 
-def updateResult(host, snmpValue, eachOIDName):
+def collectingData(host):
     snmpResults = {
         'hostIP' : None,
+        'timeCollected' : secondsTime(),
         'upsBaseBatteryTimeOnBattery': {
             'value' : None,
             'status' : None
@@ -43,6 +64,10 @@ def updateResult(host, snmpValue, eachOIDName):
             'status' : None
         },
         'upsSecondsOnBattery' : {
+            'value' : None,
+            'status' : None
+        },
+        'upsBatteryStatus' : {
             'value' : None,
             'status' : None
         },
@@ -59,39 +84,52 @@ def updateResult(host, snmpValue, eachOIDName):
             'status' : None
         }
     }
-    snmpResults['hostIP'] = host
-    if 'No response' in snmpValue:
-        snmpResults[eachOIDName]['status'] = 'DOWN'
-    if eval (snmpValue.strip() + snmpMIBS[eachOIDName]['expectValue']):
-        snmpResults[eachOIDName]['status'] = 'GOOD'
-        snmpResults[eachOIDName]['value'] = snmpValue.strip()
-    else:
-        snmpResults[eachOIDName]['status'] = 'BAD'
-        snmpResults[eachOIDName]['value'] = snmpValue.strip()
+    oidToCheck = ['upsBaseBatteryTimeOnBattery','upsSecondsOnBattery','upsSmartInputLineVoltage', 'upsInputVoltage1']
+    for eachOIDName, thisDict in snmpMIBS.items(): #name:value pair to fill in the results
+        if eachOIDName in oidToCheck:
+            #snmpFolder = 'C:\\Users\\16899486_admin\\Desktop\\C830G\\UPS\\Net-SNMP-Install\\bin\\'
+            snmpFolder = '/usr/bin/'
+            snmpCMD = [snmpFolder + 'snmpget']
+            snmpARG = ['-v', '2c', '-c', 'public', '-OQ', '-t', '10', '-r', '2']
+            for str in snmpARG:
+                snmpCMD.append(str)
+
+            snmpCMD.append(host.strip())
+            snmpCMD.append(thisDict['oidString'])
+            snmpResults['timeCollected'] = secondsTime()
+            querySNMP = Popen( snmpCMD , stdout=PIPE, stderr=PIPE)
+            stdout, stderr = querySNMP.communicate()
+
+            if len(stdout) > 0:
+                snmpValue = stdout.decode('utf-8').split('=')[-1].replace('\n', '').strip()                
+                snmpResults['hostIP'] = host
+                if eval (snmpValue.strip() + snmpMIBS[eachOIDName]['expectValue']):
+                    snmpResults[eachOIDName]['status'] = 'GOOD'
+                    snmpResults[eachOIDName]['value'] = snmpValue.strip()
+                else:
+                    snmpResults[eachOIDName]['status'] = 'BAD'
+                    snmpResults[eachOIDName]['value'] = snmpValue.strip()
+            elif len(stderr) > 0:                
+                snmpError = stderr.decode('utf-8').split('=')[-1].replace('\n', '').strip()
+                snmpResults['hostIP'] = host
+                if 'No Response from' in snmpError:
+                    snmpResults[eachOIDName]['status'] = 'DOWN/INVALID'
+                    snmpResults[eachOIDName]['value'] = snmpError.strip()
+    snmpResults = checkBattery(host, snmpResults)
     return snmpResults
 
 
 def main(hostList):
+    listResult = []
     for host in hostList:
         print ('Collecting data from host: ' + host)
         snmpResults = collectingData(host)
-        time.sleep(1)
         listResult.append(snmpResults)
+        time.sleep(1)        
     return listResult
 
-def checkBattery(host, snmpGet):
-    upsBatteryStatus = snmpGet + host + ' .1.3.6.1.2.1.33.1.2.1.0' 
-    # INTEGER {unknown(1),batteryNormal(2),batteryLow(3),batteryDepleted(4)
-    if upsBatteryStatus == 1:
-        return 'Battery Status: Unknown'
-    elif upsBatteryStatus == 2:
-        return 'Battery Status: Normal'
-    elif upsBatteryStatus == 3:
-        return 'Battery Status: Low'
-    elif upsBatteryStatus == 4:
-        return 'Battery Status: Depleted'
 
-def testFile(filenames):
+def testFile(filenames = ('good_IP_175.50.44.1.txt', 'bad_IP_175.50.44.1.txt')):
     getAllResuls = [None] * len(filenames)
     for idx in range(len(filenames)):
         snmpResults = {
@@ -142,9 +180,11 @@ def testFile(filenames):
                         snmpResults[eachOIDName]['status'] = 'BAD'
                         snmpResults[eachOIDName]['value'] = snmpValue.strip()
         getAllResuls[idx] = snmpResults
-    return getAllResuls
+    with open('result.json', 'w') as f:
+        f.writelines(json.dumps(getAllResuls, indent=4, separators=(", ", " : ")))
 
 if __name__ == '__main__':
+    # upsBatteryVoltage  .1.3.6.1.2.1.33.1.2.5  The magnitude of the present battery voltage. keeps decreasing. drop by 50
     hostList = ('175.50.44.1', '176.50.44.1')
     snmpMIBS = {
         'upsBaseBatteryTimeOnBattery': {
@@ -185,23 +225,13 @@ if __name__ == '__main__':
         }
     }
 
-    snmpFolder = '/usr/bin/'
-    snmpCMD = [snmpFolder + 'snmpget']
-    snmpARG = ['-v', '2c', '-c', 'public', '-OQ', '-t', '10', '-r', '2']
-    for str in snmpARG:
-        snmpCMD.append(str)
-
-    listResult = [{'startTimeCollect': secondsTime()}]
     listResult = main(hostList)
-    # getAllResuls = testFile(filenames = ('good_IP_175.50.44.1.txt', 'bad_IP_175.50.44.1.txt') )
-    # with open('result.json', 'w') as f:
-    #     f.writelines(json.dumps(getAllResuls, indent=4, separators=(", ", " : ")))
 
-    resultedFile = 'statusUPS.json'
+    resultedFile = './statusUPS.json'
     if os.path.isfile(resultedFile):
         if checkLogFileSize(resultedFile):
-            with open('statusUPS.json', 'w') as f:
+            with open(resultedFile, 'w+') as f:
                 f.writelines(json.dumps(listResult, indent=4, separators=(", ", " : ")))
         else:
-            with open('statusUPS.json', 'a') as f:
+            with open(resultedFile, 'a+') as f:
                 f.writelines(json.dumps(listResult, indent=4, separators=(", ", " : ")))
